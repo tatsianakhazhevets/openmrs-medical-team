7ce1fd10e3f5569d62e6683bee8795246f087a9c
Denis Bab <114239996+dennisjblake@users.noreply.github.com>
2026-09-15 14:53:16 -0400

    api changes

--- src/main/java/apiParts/skelethon/endpoints/Endpoint.java
+++ src/main/java/apiParts/skelethon/endpoints/Endpoint.java
@@ -32,7 +32,7 @@ public enum Endpoint {
             CreatePatientRequest.class,
             CreatePatientResponse.class),
     LIST_ORDERS_GET(
-            "/openmrs/ws/rest/v1/order",
+            "/order",
             BaseModel.class,
             ListOrdersResponse.class),
 
--- src/main/java/apiParts/skelethon/endpoints/order/OrderSearchEndpoint.java
+++
@@ -1,7 +0,0 @@
-package apiParts.skelethon.endpoints.order;
-import apiParts.models.order.OrderSearchParams;
-import io.restassured.response.Response;
-
-public interface OrderSearchEndpoint {
-    Response search(OrderSearchParams params);
-}
